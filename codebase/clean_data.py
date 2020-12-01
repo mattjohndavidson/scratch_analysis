0fecb197ebad34d5efb96d848692391f6c3bb62b
Matt Davidson <mattjd@uw.edu>
2020-12-01 14:00:27 -0800

    changed travis settings to build if linting fails

--- codebase/clean_data.py
+++ codebase/clean_data.py
@@ -1,14 +1,19 @@
-import pandas as pd
-import numpy as np
+"""
+Module with single function to clean data file
+"""
 
 def clean_data(data):
+    """
+    Cleans up data file for further processing.
+    """
+
     #replacing dashes with underscores
     data.columns = [i.replace('-', '_') for i in data.columns]
 
     #removing redundant columns
-    data = data.drop(columns=['Script_ID','p_ID'])
+    data = data.drop(columns=['Script_ID', 'p_ID'])
 
     #remove unnecessary columns
     data = data[data.columns[~data.columns.str.contains('param')]]
 
-    return(data)
+    return data
