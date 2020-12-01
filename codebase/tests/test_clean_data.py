e3c3d7e8f20cef4cd97ec7ca09268c04d5e475ec
Matt Davidson <mattjd@uw.edu>
2020-12-01 12:26:05 -0800

    trying to fix import on test_clean_data

--- codebase/tests/test_clean_data.py
+++ codebase/tests/test_clean_data.py
@@ -2,7 +2,7 @@ import os
 import unittest
 import pandas as pd
 import numpy as np
-import codebase.clean_data
+import clean_data
 
 data_path = os.path.join(codebase.__path__[0], 'data')
 
@@ -15,7 +15,6 @@ class test_clean_data(unittest.TestCase):
         df = pd.read_csv(os.path.join(data_path, 'project_db_1000.csv'))
         clean_dat = clean_data(df)
         dataset = df.columns
-
 
         for i in range(len(dataset)):
             has_dash = dataset[i].find("-")
