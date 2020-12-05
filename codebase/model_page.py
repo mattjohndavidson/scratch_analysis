49ace0788240340c946e22e1cc863dece3bf07c7
Matt Davidson <mattjd@uw.edu>
2020-12-05 13:32:55 -0800

    sort of works graph is ugly and page-wide gets an error with children need to understand

--- codebase/model_page.py
+++ codebase/model_page.py
@@ -14,21 +14,22 @@ Reads in saved model object; creates dash page
 """
 
 #loading the fitted model object
-model = joblib.load('fitted_model.sav')
-
+model = joblib.load('exports/fitted_model.sav')
+feature_list = joblib.load('exports/feature_list.sav')
 #create the default feature importance graph
 def get_feature_graph(label):
     """
     Creates a graph of feature importance based on passed in label
     """
-    feature_imps = pd.DataFrame(model.estimators_[label].feature_importances_*100,columns=["Importance"],index=model.feature_names)
-    feature_imps = feature_imps.sort_values("Importance", ascending=False)
+    feature_imps = pd.DataFrame(model.estimators_[label].feature_importances_*100,columns=["Importance"],index=feature_list)
+    feature_imps = feature_imps.sort_values("Importance", ascending=False)[-10:]
 
     fig_features_importance = go.Figure()
-    fig_features_importance.add_trace(go.Bar(x = feature_imps.index,
-                                            y = feature_imps["Importance"],
-                                            marker_color='rgb(171, 226, 251)'))
-    fig_features_importance.update_layout(title_text='<b>Importance of features in the model<b>', title_x = 0.5)
+    fig_features_importance.add_trace(go.Bar(x = feature_imps["Importance"],
+                                            y = feature_imps.index,
+                                            orientation = 'h',
+                                            marker_color = 'rgb(171, 226, 251)'))
+    fig_features_importance.update_layout(title_text = '<b>Importance of features in the model<b>', title_x = 0.5)
     return fig_features_importance
 
 # begin app
@@ -61,8 +62,8 @@ app.layout = html.Div(
     Output('dd-output-container', 'children'),
     [Input('labels-dropdown', 'value')])
 def update_graph(value):
-    get_feature_graph(value)
-    return fig_features_importance
+    fig = get_feature_graph(value)
+    return fig
 
 
 if __name__ == '__main__':
