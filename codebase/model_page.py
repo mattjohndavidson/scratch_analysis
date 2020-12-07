550dc2b9d5b94c26a338fe3d264b0d3435e289a2
Matt Davidson <mattjd@uw.edu>
2020-12-06 20:09:27 -0800

    currently working: displays graph of feature imp for chosen outcome

--- codebase/model_page.py
+++ codebase/model_page.py
@@ -1,70 +1,63 @@
 import pandas as pd
 import numpy as np
 import joblib
-import plotly.graph_objects as go
+import plotly.express as px
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 #import dash_daq as dash_daq
 from dash.dependencies import Input, Output
+import os
 
 """
 Reads in saved model object; creates dash page
-
 """
 
 #loading the fitted model object
-model = joblib.load('exports/fitted_model.sav')
-feature_list = joblib.load('exports/feature_list.sav')
-#create the default feature importance graph
-def get_feature_graph(label):
-    """
-    Creates a graph of feature importance based on passed in label
-    """
-    feature_imps = pd.DataFrame(model.estimators_[label].feature_importances_*100,columns=["Importance"],index=feature_list)
-    feature_imps = feature_imps.sort_values("Importance", ascending=False)[-10:]
-
-    fig_features_importance = go.Figure()
-    fig_features_importance.add_trace(go.Bar(x = feature_imps["Importance"],
-                                            y = feature_imps.index,
-                                            orientation = 'h',
-                                            marker_color = 'rgb(171, 226, 251)'))
-    fig_features_importance.update_layout(title_text = '<b>Importance of features in the model<b>', title_x = 0.5)
-    return fig_features_importance
+dirname = os.path.dirname(__file__)
+filename_model = os.path.join(dirname, 'exports/fitted_model.sav')
+filename_features = os.path.join(dirname, 'exports/feature_list.sav')
+model = joblib.load(filename_model)
+feature_list = joblib.load(filename_features)
 
 # begin app
-
 app = dash.Dash(__name__)
-app.layout = html.Div(
-    children = [
-        # title
-        html.H1("See which features were most predictive of the chosen popularity metric"),
-            
-        dcc.Graph(figure=get_feature_graph(0)),
-        
-        #dropdown menu to choose feature importance graph based on one of 4 labels
-        dcc.Dropdown(
-            id='labels-dropdown',
-            options=[
-                {'label': 'Total remixes', 'value': '0'},
-                {'label': 'Total views', 'value': '1'},
-                {'label': 'Total favorties', 'value': '2'},
-                {'label': 'Total loves', 'value': '3'}
-            ],
-            value='0'
-        ),
-        html.Div(id='dd-output-container')
-    ]
-)
+app.layout = html.Div(style={'textAlign': 'center', 'width': '800px', 'font-family': 'Verdana'},
 
+    children = [    
+    # title
+    html.H1("See which features were most predictive of the chosen popularity metric"),
+
+    #dropdown menu to choose feature importance graph based on one of 4 labels
+    dcc.Dropdown(
+        id='labels-dropdown',
+        options=[
+            {'label': 'Total remixes', 'value': 0},
+            {'label': 'Total views', 'value': 1},
+            {'label': 'Total favorties', 'value': 2},
+            {'label': 'Total loves', 'value': 3}
+        ],
+        value=0
+    ),
+
+    dcc.Graph(id = 'figure_importances')
+])
 
 @app.callback(
-    Output('dd-output-container', 'children'),
+    Output('figure_importances', 'figure'),
     [Input('labels-dropdown', 'value')])
-def update_graph(value):
-    fig = get_feature_graph(value)
-    return fig
+def update_graph(outcome):
+    feature_imps = pd.DataFrame(model.estimators_[outcome].feature_importances_,columns=["Importance"],index=feature_list)
+    feature_imps = feature_imps.sort_values("Importance", ascending=False)
 
+    fig = px.bar(x = feature_imps["Importance"][:10], y = feature_imps.index[:10], orientation='h',
+                 range_x=(0, .25),
+                 labels={
+                     'x' : 'Feature Importance',
+                     'y' : 'Feature Name'
+                 })
+    fig.update_layout(yaxis={'categoryorder':'total ascending'})
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
