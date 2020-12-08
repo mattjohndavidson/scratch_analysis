522e130b930aa29e664c755bd69db53d3a8901ff
Jacob Cohen <jtcohen@uw.edu>
2020-12-07 18:16:45 -0800

    Add 2 unit tests

--- codebase/tests/test_clean_data.py
+++ codebase/tests/test_clean_data.py
@@ -3,6 +3,7 @@ import unittest
 import pandas as pd
 import numpy as np
 from codebase import clean_data
+from codebase import search
 
 #print(clean_data.__path__)
 data_path = "data/"
@@ -22,3 +23,28 @@ class test_clean_data(unittest.TestCase):
    #     for i in range(len(dataset)):
    #         has_dash = dataset[i].find("-")
    #         self.assertNotEqual(has_dash, -1)
+    
+
+class test_search(unittest.TestCase):
+    """Includes unit tests for search.py"""
+    
+    def test1(self):
+        """column input is not a string"""
+        with self.assertRaises(ValueError):
+            data = clean_data.clean_columns(pd.read_csv('codebase/data/project_db_1000.csv', low_memory=False))
+            column = 4
+            block_search = None
+            search.search_data(data,column,block_search)
+
+
+    def test2(self):
+        """column input is not an option"""
+        with self.assertRaises(ValueError):
+            data = clean_data.clean_columns(pd.read_csv('codebase/data/project_db_1000.csv', low_memory=False))
+            column = 'Number'
+            block_search = None
+            search.search_data(data,column,block_search)
+
+
+if __name__ == '__main__':
+    unittest.main()
