dd53cbdfebc39e7221c2d149f97781039d1976f5
Matt Davidson <mattjd@uw.edu>
2020-12-07 20:19:06 -0800

    Revert "Add 2 unit tests"
    This reverts commit 522e130b930aa29e664c755bd69db53d3a8901ff.

--- codebase/search.py
+++ codebase/search.py
@@ -1,5 +1,3 @@
-"""Contains function to search scratch data."""
-
 import numpy as np
 import pandas as pd
 
@@ -10,33 +8,30 @@ def search_data(data, column, block_search=np.array([None])):
     --------
     data: pandas dataframe cleaned with clean_data.py
     column: column to sort by
-    block_type: if column = block_type, block_type defines which block_types
-                to include in the search, default is None
-
+    block_type: if column = block_type, block_type defines which block_types to include in the search, default is None
+    
     Returns
     --------
     sorted_data: pandas sorted dataframe sorted by user choice.
     """
-    if not isinstance(column, str):
+    if not isinstance(column,str):
         raise ValueError('Bad input. Column must be a string.')
     else:
         pass
     if block_search is None:
         pass
-    elif not all(isinstance(block, str) for block in block_search):
+    elif not all(isinstance(block,str) for block in block_search):
         raise ValueError('Bad input. block_types must be strings.')
     else:
         pass
 
     popularity_grades = data.groupby('project_ID').first()
     blocks_flattened = data.groupby(['project_ID'])['block_type'].value_counts().unstack(fill_value=0).reset_index()
-    merged = popularity_grades.merge(blocks_flattened, left_on='project_ID',
-                                     right_on='project_ID', how='left')
+    merged = popularity_grades.merge(blocks_flattened, left_on='project_ID', right_on='project_ID', how = 'left')
 
-    removed_columns = np.array(['script_ID', 'project_ID', 'project_name',
-                                'is_remix', 'block_rank', 'Clones', 'InstancesSprites'])
-    columns = np.setdiff1d(data.columns.values, removed_columns)
-    block_types = np.setdiff1d(np.array(merged.columns.values), np.array(data.columns.values))
+    removed_columns = np.array(['script_ID','project_ID','project_name','is_remix','block_rank','Clones','InstancesSprites'])
+    columns = np.setdiff1d(data.columns.values,removed_columns)
+    block_types = np.setdiff1d(np.array(merged.columns.values),np.array(data.columns.values))
 
     if column not in columns:
         raise ValueError('Bad input. column must be a column of the dataframe.')
@@ -49,11 +44,11 @@ def search_data(data, column, block_search=np.array([None])):
             search_term = block_search[0]
         else:
             block_search_sum = sum([merged[block] for block in block_search])
-            merged.insert(len(merged.columns), "search_sum", block_search_sum)
+            merged.insert(len(merged.columns),"search_sum",block_search_sum)
             search_term = 'search_sum'
     else:
         search_term = column
 
-    sorted_data = merged.sort_values(search_term, ascending=False)
+    sorted_data = merged.sort_values(search_term,ascending=False)
 
     return sorted_data
